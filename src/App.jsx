0eeef26daacc00da171f5c2d49f236a0dc7a7748
sefeoluwa <sefeoluwa01@gmail.com>
2023-09-23 13:26:56 +0100

    created divs and added resume section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { CenterTopBar,  LeftPanel, EditContent, CustomizeLook } from "./components"
+import { CenterTopBar,  LeftPanel, EditContent, CustomizeLook, Resume } from "./components"
 
 function App() {
   return (
-    <>
+    <div className="flex p-10 justify-between">
     <LeftPanel />
+    <div className="">
     <CenterTopBar />
     <EditContent />
     <CustomizeLook />
-   
-    </>
+    </div>
+    <Resume />
+    </div>
   )
 }
 
