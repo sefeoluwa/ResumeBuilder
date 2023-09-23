bf7caf9e7efda1923fd532daa8f344c02dab8b09
sefeoluwa <sefeoluwa01@gmail.com>
2023-09-23 14:49:20 +0100

    added icons to buttons

--- src/components/LeftPanel.jsx
+++ src/components/LeftPanel.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { FaRegNewspaper} from 'react-icons/fa'
+import { BsBorderStyle } from 'react-icons/bs'
 
 function leftPanel() {
   return (
-    <div>leftPanel</div>
+    <div className='flex flex-col justify-between h-[15vh] p-2 w-[18%]'>
+      <button className='flex'>
+        <FaRegNewspaper 
+        style={{
+          marginTop: '4px'
+        }}
+        />
+        <p className='ml-[5px] text-[18px]'>Content</p>
+        </button>
+      <button className='flex'> 
+        <BsBorderStyle
+         style={{
+          marginTop: '4px'
+        }}
+         />
+         <p className='ml-[5px] text-[18px]'>Customize</p>
+         </button>
+    </div> 
   )
 }
 
