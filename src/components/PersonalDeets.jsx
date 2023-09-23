e822ab9f35c2088d894576270e364d38e27a972e
sefeoluwa <sefeoluwa01@gmail.com>
2023-09-23 16:54:42 +0100

    added links form

--- src/components/PersonalDeets.jsx
+++ src/components/PersonalDeets.jsx
@@ -10,22 +10,46 @@ function personalDeets() {
       <label htmlFor="name">Full Name</label>
         <input type="text" name="name" id="name" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' required/>
     </div>
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2 mt-3">
       <label htmlFor="email">Email <span className='text-[11px] text-gray-500 font-bold' >recommended</span> </label>
         <input type="email" name="email" id="email" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' />
     </div>
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2 mt-3">
       <label htmlFor="number">Phone Number <span className='text-[11px] text-gray-500 font-bold' >recommended</span></label>
-        <input type="number" name="number" id="number" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' />
+        <input type="tel" name="number" id="number" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' />
     </div>
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2 mt-3">
       <label htmlFor="address">Address <span className='text-[11px] text-gray-500 font-bold' >recommended</span></label>
         <input type="text" name="address" id="address" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' />
     </div>
+    <div className="flex flex-col gap-2 mt-3">
+      <label htmlFor="title">Job Title <span className='text-[11px] text-gray-500 font-bold' >optional</span></label>
+        <input type="text" name="title" id="title" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]'/>
+    </div>
     </form>
 
-    <p>Social Profiles</p>
+    <p className='font-bold text-[18px] pl-2 mt-3'>Social Profiles</p>
+    <form action="" className='mt-4 pl-2'>
+    <div className="flex flex-col gap-2">
+      <label htmlFor="title">LinkedIn <span className='text-[11px] text-gray-500 font-bold' >optional</span></label>
+        <input type="text" name="title" id="title" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' placeholder="https://www.linkedin.com/in/johndoe/"/>
+    </div>
 
+    <div className="flex flex-col gap-2 mt-3">
+      <label htmlFor="github">GitHub <span className='text-[11px] text-gray-500 font-bold' >optional</span></label>
+        <input type="text" name="github" id="github" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' placeholder="https://www.github.com/johndoe/"/>
+    </div>
+
+    <div className="flex flex-col gap-2 mt-3">
+      <label htmlFor="twitter">Twitter <span className='text-[11px] text-gray-500 font-bold' >optional</span></label>
+        <input type="text" name="twitter" id="twitter" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' placeholder="https://www.linkedin.com/in/johndoe/"/>
+    </div>
+
+    <div className="flex flex-col gap-2 mt-3">
+      <label htmlFor="website">Website <span className='text-[11px] text-gray-500 font-bold' >optional</span></label>
+        <input type="text" name="website" id="website" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' placeholder="https://www.johndoe.com"/>
+    </div>
+    </form>
     </div>
   )
 }
