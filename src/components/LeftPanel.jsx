8a02d3d10e8ff957c894d137f99598699a3df4f3
sefeoluwa <sefeoluwa01@gmail.com>
2023-09-23 15:22:42 +0100

    set state for background color

--- src/components/LeftPanel.jsx
+++ src/components/LeftPanel.jsx
@@ -1,27 +1,32 @@
-import { FaRegNewspaper} from 'react-icons/fa'
-import { BsBorderStyle } from 'react-icons/bs'
+import { FaRegNewspaper } from 'react-icons/fa';
+import { BsBorderStyle } from 'react-icons/bs';
+import { useState } from 'react';
 
-function leftPanel() {
+function LeftPanel() {
+  const [activeButton, setActiveButton] = useState('content');
+
+  const handleButtonClick = (buttonName) => {
+    setActiveButton(buttonName);
+  };
+
   return (
-    <div className='flex flex-col justify-between h-[15vh] p-2 w-[18%]'>
-      <button className='flex'>
-        <FaRegNewspaper 
-        style={{
-          marginTop: '4px'
-        }}
-        />
+    <div className='flex flex-col justify-between h-[15vh] p-2 w-[10vw] bg-secondary'>
+      <button
+        className={`flex flex-col items-center rounded-[10px] ${activeButton === 'content' ? 'bg-primary' : ''}`}
+        onClick={() => handleButtonClick('content')}
+      >
+        <FaRegNewspaper style={{ marginTop: '4px' }} />
         <p className='ml-[5px] text-[18px]'>Content</p>
-        </button>
-      <button className='flex'> 
-        <BsBorderStyle
-         style={{
-          marginTop: '4px'
-        }}
-         />
-         <p className='ml-[5px] text-[18px]'>Customize</p>
-         </button>
-    </div> 
-  )
+      </button>
+      <button
+        className={`flex flex-col items-center rounded-[10px] ${activeButton === 'customize' ? 'bg-primary' : ''}`}
+        onClick={() => handleButtonClick('customize')}
+      >
+        <BsBorderStyle style={{ marginTop: '4px' }} />
+        <p className='ml-[5px] text-[18px]'>Customize</p>
+      </button>
+    </div>
+  );
 }
 
-export default leftPanel
+export default LeftPanel;
