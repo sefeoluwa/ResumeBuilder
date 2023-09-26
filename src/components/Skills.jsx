f25770c13cc8e7ac43f2ea4022e837bc2cf77bfa
sefeoluwa <sefeoluwa01@gmail.com>
2023-09-26 12:03:19 +0100

    skills heading

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { GiSkills } from 'react-icons/gi' 
+import { VscTriangleDown } from 'react-icons/vsc'
 
 function skills() {
   return (
-    <div>skills</div>
+    <div className='p-3 bg-secondary mt-4 rounded-[10px] pb-6'>
+     <button className="flex justify-between pl-2 pt-3 w-full">
+      <div className='flex font-bold text-[18px] mt-[-4px] pb-2'> 
+        <GiSkills
+        style={{
+          height: '30px',
+          width: '30px'
+        }}
+        /> 
+        <h3 className='pl-2'>Skills</h3>
+      </div>
+
+      <VscTriangleDown className='' />
+    </button>
+    </div>
   )
 }
 
