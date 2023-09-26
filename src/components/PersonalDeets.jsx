2002d49a060ba234243424dde1c818866e63becd
sefeoluwa <sefeoluwa01@gmail.com>
2023-09-26 11:56:10 +0100

    separated sections

--- src/components/PersonalDeets.jsx
+++ src/components/PersonalDeets.jsx
@@ -2,55 +2,58 @@ import { VscTriangleDown } from 'react-icons/vsc'
 
 function personalDeets() {
   return (
+    <>
     <div className='p-3 bg-secondary mt-4 rounded-[10px]'>
     <button className='font-bold text-[20px] pl-2 flex justify-between w-full'>Personal Details <span > <VscTriangleDown /></span></button>
 
     <form action="" className='mt-4 pl-2'>
     <div className="flex flex-col gap-2">
       <label htmlFor="name">Full Name</label>
-        <input type="text" name="name" id="name" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' required/>
+        <input type="text" name="name" id="name" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' placeholder='John Doe' required/>
     </div>
     <div className="flex flex-col gap-2 mt-3">
       <label htmlFor="email">Email <span className='text-[11px] text-gray-500 font-bold' >recommended</span> </label>
-        <input type="email" name="email" id="email" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' />
+        <input type="email" name="email" id="email" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' placeholder='johndoe@gmail.com' />
     </div>
     <div className="flex flex-col gap-2 mt-3">
       <label htmlFor="number">Phone Number <span className='text-[11px] text-gray-500 font-bold' >recommended</span></label>
-        <input type="tel" name="number" id="number" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' />
+        <input type="tel" name="number" id="number" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' placeholder='Phone' />
     </div>
     <div className="flex flex-col gap-2 mt-3">
       <label htmlFor="address">Address <span className='text-[11px] text-gray-500 font-bold' >recommended</span></label>
-        <input type="text" name="address" id="address" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' />
+        <input type="text" name="address" id="address" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' placeholder='Lagos, Nigeria' />
     </div>
     <div className="flex flex-col gap-2 mt-3">
       <label htmlFor="title">Job Title <span className='text-[11px] text-gray-500 font-bold' >optional</span></label>
-        <input type="text" name="title" id="title" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]'/>
+        <input type="text" name="title" id="title" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' placeholder='Frontend Engineer'/>
     </div>
-    </form>
+    </form>   
+    </div>
+<div className="p-3 bg-secondary mt-4 rounded-[10px]">
+<button className='w-full font-bold text-[18px] pl-2 mt-3 flex justify-between'>Social Profiles <span > <VscTriangleDown /></span></button>
+<form action="" className='mt-4 pl-2'>
+<div className="flex flex-col gap-2">
+  <label htmlFor="title">LinkedIn <span className='text-[11px] text-gray-500 font-bold' >optional</span></label>
+    <input type="url" name="title" id="title" className='bg-primary outline-none pl-2 rounded-[10purl-[14px] h-[40px]' placeholder="https://www.linkedin.com/in/johndoe/"/>
+</div>
 
-    <button className='w-full font-bold text-[18px] pl-2 mt-3 flex justify-between'>Social Profiles <span > <VscTriangleDown /></span></button>
-    <form action="" className='mt-4 pl-2'>
-    <div className="flex flex-col gap-2">
-      <label htmlFor="title">LinkedIn <span className='text-[11px] text-gray-500 font-bold' >optional</span></label>
-        <input type="url" name="title" id="title" className='bg-primary outline-none pl-2 rounded-[10purl-[14px] h-[40px]' placeholder="https://www.linkedin.com/in/johndoe/"/>
-    </div>
+<div className="flex flex-col gap-2 mt-3">
+  <label htmlFor="github">GitHub <span className='text-[11px] text-gray-500 font-bold' >optional</span></label>
+    <input type="url" name="github" id="github" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' placeholder="https://www.github.com/johndoe/"/>
+</div>
 
-    <div className="flex flex-col gap-2 mt-3">
-      <label htmlFor="github">GitHub <span className='text-[11px] text-gray-500 font-bold' >optional</span></label>
-        <input type="url" name="github" id="github" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' placeholder="https://www.github.com/johndoe/"/>
-    </div>
+<div className="flex flex-col gap-2 mt-3">
+  <label htmlFor="twitter">Twitter <span className='text-[11px] text-gray-500 font-bold' >optional</span></label>
+    <input type="url" name="twitter" id="twitter" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' placeholder="https://www.linkedin.com/in/johndoe/"/>
+</div>
 
-    <div className="flex flex-col gap-2 mt-3">
-      <label htmlFor="twitter">Twitter <span className='text-[11px] text-gray-500 font-bold' >optional</span></label>
-        <input type="url" name="twitter" id="twitter" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' placeholder="https://www.linkedin.com/in/johndoe/"/>
-    </div>
-
-    <div className="flex flex-col gap-2 mt-3">
-      <label htmlFor="website">Website <span className='text-[11px] text-gray-500 font-bold' >optional</span></label>
-        <input type="url" name="website" id="website" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' placeholder="https://www.johndoe.com"/>
-    </div>
-    </form>
-    </div>
+<div className="flex flex-col gap-2 mt-3">
+  <label htmlFor="website">Website <span className='text-[11px] text-gray-500 font-bold' >optional</span></label>
+    <input type="url" name="website" id="website" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' placeholder="https://www.johndoe.com"/>
+</div>
+</form>
+</div>
+</>
   )
 }
 
