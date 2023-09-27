e30cdda5f1788b40f39852751f2c578003ea073c
sefeoluwa <sefeoluwa01@gmail.com>
2023-09-27 14:54:34 +0100

    shifted socials and personal details to new component

--- src/components/PersonalDeets.jsx
+++ src/components/PersonalDeets.jsx
@@ -1,11 +1,10 @@
+/* eslint-disable react-refresh/only-export-components */
 import { VscTriangleDown } from 'react-icons/vsc'
+import { BsPersonBoundingBox } from 'react-icons/bs'
+import { useState } from 'react'
 
-function personalDeets() {
-  return (
-    <>
-    <div className='p-3 pb-6 bg-secondary mt-4 rounded-[10px]'>
-    <button className='font-bold text-[20px] pl-2 flex justify-between w-full'>Personal Details <span > <VscTriangleDown /></span></button>
-
+const PersonalCard = () => {
+  return(
     <form action="" className='mt-4 pl-2'>
     <div className="flex flex-col gap-2">
       <label htmlFor="name">Full Name</label>
@@ -28,10 +27,41 @@ function personalDeets() {
         <input type="text" name="title" id="title" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' placeholder='Frontend Engineer'/>
     </div>
     </form>   
+  )
+}
+
+const Personal = () => {
+  const [persCardVisible, setPersCardVisible] = useState(false)
+  const handleArrowBtnClick = () => {
+    setPersCardVisible(!persCardVisible)
+  }
+
+  return(
+    <div className='p-3 pb-6 bg-secondary mt-4 rounded-[10px]'>
+      <button 
+      className='font-bold text-[20px] pl-2 pt-3 flex justify-between w-full'
+      onClick={handleArrowBtnClick}
+      >
+        <div className='flex font-bold text-[18px] mt-[-4px] pb-2'> 
+        <BsPersonBoundingBox
+        style={{
+          height: '28px',
+          width: '30px'
+        }}
+        /> 
+        <h3 className='pl-2'>Personal Details</h3>
+      </div>
+      <VscTriangleDown className='' />
+     </button>
+
+    {persCardVisible && <PersonalCard />}
     </div>
-<div className="p-3 pb-6 bg-secondary mt-4 rounded-[10px]">
-<button className='w-full font-bold text-[18px] pl-2 mt-3 flex justify-between'>Social Profiles <span > <VscTriangleDown /></span></button>
-<form action="" className='mt-4 pl-2'>
+  )
+}
+
+const SocialsCard = () => {
+  return(
+    <form action="" className='mt-4 pl-2'>
 <div className="flex flex-col gap-2">
   <label htmlFor="title">LinkedIn <span className='text-[11px] text-gray-500 font-bold' >optional</span></label>
     <input type="url" name="title" id="title" className='bg-primary outline-none pl-2 rounded-[10purl-[14px] h-[40px]' placeholder="https://www.linkedin.com/in/johndoe/"/>
@@ -52,8 +82,38 @@ function personalDeets() {
     <input type="url" name="website" id="website" className='bg-primary outline-none pl-2 rounded-[10px] text-[14px] h-[40px]' placeholder="https://www.johndoe.com"/>
 </div>
 </form>
-</div>
-</>
+  )
+}
+
+const Socials = () => {
+  const [socialCardVisible, setSocialCardVisible] = useState(false)
+
+  const handleArrowBtnClick = () => {
+    setSocialCardVisible(!socialCardVisible)
+  }
+ 
+  return(
+    <div className="p-3 pb-6 bg-secondary mt-4 rounded-[10px]">
+      <button 
+      className='w-full font-bold text-[18px] pl-2 mt-3 flex justify-between'
+      onClick={handleArrowBtnClick}
+      >
+      Social Profiles 
+      <span > <VscTriangleDown /></span>
+      </button>
+
+      {socialCardVisible && <SocialsCard />}
+
+    </div>
+  )
+}
+
+function personalDeets() {
+  return (
+    <>
+    <Personal />
+    <Socials />
+    </>
   )
 }
 
