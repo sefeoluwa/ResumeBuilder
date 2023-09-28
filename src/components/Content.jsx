b7a2e4a21b25a6acb25f5bd40917c3b4ff4cb12f
sefeoluwa <sefeoluwa01@gmail.com>
2023-09-28 18:00:20 +0100

    renamed components

---
+++ src/components/Content.jsx
@@ -0,0 +1,14 @@
+import { Education, Experience, Skills, PersonalDeets } from "."
+
+function Content() {
+  return (
+   <>
+    <PersonalDeets />
+    <Education />
+    <Skills />
+    <Experience />
+   </>
+  )
+}
+
+export default Content
