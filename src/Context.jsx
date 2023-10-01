49d995eb17738c6c151e92fc68708ab819cc5540
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-01 14:42:54 +0100

    addded context Api

--- src/Context.jsx
+++ src/Context.jsx
@@ -5,8 +5,26 @@ import { createContext, useState } from "react"
 const DataContext = createContext()
 
 export const DataProvider = ({ children }) => {
+
+    const [personalData, setPersonalData] = useState({
+        fullName: '',
+        email: '',
+        number: '',
+        address: '',
+        title: '',
+    })
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setPersonalData((prevData) => ({
+          ...prevData,
+          [name]: value,
+        }));
+        
+      };
+
     return(
-        <DataContext.Provider value={{ data: 1 }}>{children}</DataContext.Provider>
+        <DataContext.Provider value={{ personalData, setPersonalData, handleInputChange }}>{children}</DataContext.Provider>
     )
 }
 
