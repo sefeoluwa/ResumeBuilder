ae7c6bf2b2cf1f2bc5f18c72660f03d87ae93a4e
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-01 15:36:49 +0100

    added content to the Details section of resume

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { useContext } from 'react'
+import DataContext from '../Context'
 
 function Details() {
+  const { personalData } = useContext(DataContext)
   return (
-    <div>Details</div>
+    <div>
+     <h1>{personalData.fullName}</h1>
+     <p>{personalData.email}</p>
+     <p>{personalData.number}</p>
+     <p>{personalData.address}</p>
+     <p>{personalData.title}</p>
+    </div>
   )
 }
 
