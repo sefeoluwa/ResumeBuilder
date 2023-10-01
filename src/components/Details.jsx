7fda45fc62d06c53836cbdf048e0fdb0ab5e1cee
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-01 16:36:02 +0100

    added social links section

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -2,8 +2,9 @@ import { useContext } from 'react'
 import DataContext from '../Context'
 
 function Details() {
-  const { personalData } = useContext(DataContext)
+  const { personalData, socialData } = useContext(DataContext)
   return (
+    <>
     <div>
      <h1>{personalData.fullName}</h1>
      <p>{personalData.email}</p>
@@ -11,6 +12,22 @@ function Details() {
      <p>{personalData.address}</p>
      <p>{personalData.title}</p>
     </div>
+    <div className="flex justify-between">
+  <a href={socialData.linkedin} target="_blank" rel="noopener noreferrer">
+    LinkedIn
+  </a>
+  <a href={socialData.github} target="_blank" rel="noopener noreferrer">
+    GitHub
+  </a>
+  <a href={socialData.twitter} target="_blank" rel="noopener noreferrer">
+    Twitter
+  </a>
+  <a href={socialData.website} target="_blank" rel="noopener noreferrer">
+    Website
+  </a>
+</div>
+
+    </>
   )
 }
 
