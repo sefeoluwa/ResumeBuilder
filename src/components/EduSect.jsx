914899251a5acb540f4a2588eb081c8244838dcf
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-04 11:27:38 +0100

    added form details render

--- src/components/EduSect.jsx
+++ src/components/EduSect.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { useContext } from "react"
+import DataContext from "../Context"
 
 const EduSect = () => {
+  const { education } = useContext(DataContext); 
   return (
-    <div>EduSect</div>
+    <div>
+      {education.map((edu) => {
+        return (
+          <div className="" key={edu.degree}>
+            <h2>{edu.degree}</h2>
+            <p>{edu.school}</p>
+          </div>
+        );
+      })}
+    </div>
   )
 }
 
