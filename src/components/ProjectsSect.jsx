9dfadab0e334fe66fe231960ddb3fa17f766202c
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-04 12:55:19 +0100

    added project form details render

--- src/components/ProjectsSect.jsx
+++ src/components/ProjectsSect.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { useContext } from 'react'
+import DataContext from '../Context'
 
 const ProjectsSect = () => {
+  const { projects } = useContext(DataContext)
   return (
-    <div>ProjectsSect</div>
+    <div>
+      {projects.map((project, index) => (
+        <div className="" key={`${project.projectName}-${index}`}>
+          <h3>{project.projectName}</h3>
+          <p>{project.start}</p>
+          <p>{project.end}</p>
+          <p>{project.description}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
