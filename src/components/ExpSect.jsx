05bf8e00b60fe6b7ef7f628c6d633f94517ef6f6
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-04 12:42:01 +0100

    added experience form details render

--- src/components/ExpSect.jsx
+++ src/components/ExpSect.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { useContext } from 'react'
+import DataContext from '../Context'
 
 const ExpSect = () => {
+  const { experience } = useContext(DataContext)
+
   return (
-    <div>ExpSect</div>
+    <div>
+      {experience.map((exp, index) => (
+        <div className="" key={`${exp.exp}-${index}`}>
+          <h3>{exp.role}</h3>
+          <p>{exp.company}</p>
+          <p>{exp.start}</p>
+          <p>{exp.end}</p>
+          <p>{exp.description}</p>
+
+        </div>
+      ))}
+    </div>
   )
 }
 
