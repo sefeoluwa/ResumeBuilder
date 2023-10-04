44ae4f99eae1c59dafd670271a3568a328849d5c
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-04 21:35:56 +0100

    added logo files

---
+++ src/assets/index.js
@@ -0,0 +1,14 @@
+import logoBlack from './logoBlack.svg'
+import logoWhite from './logoWhite'
+import logoColor from './logoColor'
+import logoTransparent from './logoTransparent'
+import logo from './logo'
+
+
+export default {
+    logoBlack,
+    logoColor,
+    logoTransparent,
+    logoWhite,
+    logo,
+}
