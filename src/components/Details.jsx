0ad986e234b89483bc75b0b1ace5a991a7ffc100
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-06 14:52:30 +0100

    fixed render bug after data fetch

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,50 +1,60 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 import { db, auth } from '../firebase-config';
 import { collection, getDocs } from 'firebase/firestore';
 
 function Details() {
-  const [personalList, setPersonalList] = useState([])
-  const personalCollectionRef = collection(db, 'personalDetails')
+  const [personalList, setPersonalList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); 
+  const personalCollectionRef = collection(db, 'personalDetails');
 
   useEffect(() => {
-  const user = auth.currentUser;
-
-  if(user) {
-    const queryPersonalDetails = async () => {
-      const querySnapshot = await getDocs(personalCollectionRef);
-      const persData = [];
-      querySnapshot.forEach((doc) => {
-        const data = doc.data()
-
-        if (data.userId === user.uid) {
-          persData.push(data);
+      const queryPersonalDetails = async () => {
+        try {
+          const querySnapshot = await getDocs(personalCollectionRef);
+          const persData = [];
+          querySnapshot.forEach((doc) => {
+            const data = doc.data();
+            const user = auth.currentUser;
+            if (data.userId === user.uid) {
+              persData.push(data);
+            }
+          });
+          setPersonalList(persData);
+        } catch (error) {
+          console.error('Error fetching personal data: ', error);
+        } finally {
+          setIsLoading(false); // Set isLoading to false after the request is completed
         }
-      })
-      setPersonalList(persData)
-    }
-    queryPersonalDetails();
-  }
-  }, [personalCollectionRef])
+      };
 
+      queryPersonalDetails();
+    
+  }, [setIsLoading, personalCollectionRef]);
 
   return (
-    <>
     <div>
-    {personalList.map((personal, index) => (
-      <div className="" key={`${personal.fullName}-${index}`}>
-        <h2>{personal.fullName}</h2>
-        <p>{personal.email}</p>
-        <p>{personal.number}</p>
-        <p>{personal.address}</p>
-        <p>{personal.title}</p>
-      </div>
-    ))}
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        personalList.map((personal, index) => (
+          <div className="" key={`${personal.fullName}-${index}`}>
+            <h2>{personal.fullName}</h2>
+            <p>{personal.email}</p>
+            <p>{personal.number}</p>
+            <p>{personal.address}</p>
+            <p>{personal.title}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
+}
+
+export default Details;
 
 
 
-
-
-    {/* </div>
+ {/* </div>
     <div className="flex justify-between">
   <a href={socialData.linkedin} target="_blank" rel="noopener noreferrer">
     LinkedIn
@@ -58,10 +68,3 @@ function Details() {
   <a href={socialData.website} target="_blank" rel="noopener noreferrer">
     Website
   </a> */}
-</div>
-
-    </>
-  )
-}
-
-export default Details
