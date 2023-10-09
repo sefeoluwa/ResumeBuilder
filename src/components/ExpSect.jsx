241c54c330883b454226c0ce92f0be20fd664aa0
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-09 12:58:39 +0100

    fetching experience from db

--- src/components/ExpSect.jsx
+++ src/components/ExpSect.jsx
@@ -1,21 +1,46 @@
-import { useContext } from 'react'
-import DataContext from '../Context'
+import { useState, useEffect } from 'react';
+import { db } from '../firebase-config';
+import { collection, getDocs } from 'firebase/firestore';
 
 const ExpSect = () => {
-  const { experience } = useContext(DataContext)
+const [experience, setExperience] = useState([])
+const [isLoading, setIsLoading] = useState(null)
+const expCollectionRef = collection(db, 'experience')
+
+useEffect(() => {
+  const fetchExp = async () => {
+    try {
+      const querySnapshot = await getDocs(expCollectionRef)
+      const experienceData = []
+      querySnapshot.forEach((doc) => {
+        experienceData.push(doc.data())
+      })
+      setExperience(experienceData)
+    } catch (error) {
+      console.error('Error fetching experience data: ', error);
+    } finally {
+      setIsLoading(false); 
+    }
+  }
+  fetchExp()
+}, [expCollectionRef])
 
   return (
     <div>
-      {experience.map((exp, index) => (
-        <div className="" key={`${exp.exp}-${index}`}>
-          <h3>{exp.role}</h3>
-          <p>{exp.company}</p>
-          <p>{exp.start}</p>
-          <p>{exp.end}</p>
-          <p>{exp.description}</p>
-
-        </div>
-      ))}
+      {isLoading ? (
+        <div className="">Loading...</div>
+      ) : (
+        experience.map((exp, index) => (
+          <div className="" key={`${exp.exp}-${index}`}>
+            <h3>{exp.role}</h3>
+            <p>{exp.company}</p>
+            <p>{exp.start}</p>
+            <p>{exp.end}</p>
+            <p>{exp.description}</p>
+  
+          </div>
+        ))
+      )}
     </div>
   )
 }
