1e46478b0499da6e71b6cea806cd47a3d497cb0c
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-09 13:14:01 +0100

    fetch projects from db

--- src/components/ProjectsSect.jsx
+++ src/components/ProjectsSect.jsx
@@ -1,18 +1,45 @@
-import { useContext } from 'react'
-import DataContext from '../Context'
+import { useState, useEffect } from 'react';
+import { db } from '../firebase-config';
+import { collection, getDocs } from 'firebase/firestore';
 
 const ProjectsSect = () => {
-  const { projects } = useContext(DataContext)
+  const projectsCollectionRef = collection(db, 'projects')
+  const [projects, setProjects] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const querySnapshot = await getDocs(projectsCollectionRef)
+        const projectsData = []
+        querySnapshot.forEach((doc) => {
+          projectsData.push(doc.data())
+        })
+        setProjects(projectsData)
+      } catch (error) {
+        console.error('Error fetching skills data: ', error);
+      } finally {
+        setIsLoading(false); 
+      }
+    }
+    fetchProjects()
+  }, [projectsCollectionRef])
+
+
   return (
     <div>
-      {projects.map((project, index) => (
-        <div className="" key={`${project.projectName}-${index}`}>
-          <h3>{project.projectName}</h3>
-          <p>{project.start}</p>
-          <p>{project.end}</p>
-          <p>{project.description}</p>
-        </div>
-      ))}
+      {isLoading ? (
+        <div className="">Loading...</div>
+      ) : (
+        projects.map((project, index) => (
+          <div className="" key={`${project.projectName}-${index}`}>
+            <h3>{project.projectName}</h3>
+            <p>{project.start}</p>
+            <p>{project.end}</p>
+            <p>{project.description}</p>
+          </div>
+        ))
+      )}
     </div>
   )
 }
