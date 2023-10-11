380ea3ae6fc0b919feae1afc6b2c62746701f03f
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-11 15:33:59 +0100

    added border bottom

--- src/components/PersonalDetails.jsx
+++ src/components/PersonalDetails.jsx
@@ -2,7 +2,7 @@ import {Details} from '../components'
 
 export const PersonalDetails = () => {
   return (
-    <div className='h-[20%] bg-delete p-5'>
+    <div className='h-[20%] p-5 border-b-[5px] mb-4 '>
         <Details />
     </div>
   )
