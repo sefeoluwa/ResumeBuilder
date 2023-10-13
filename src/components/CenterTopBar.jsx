df9bcffbc92fd512e82c340b4ffd6f6bfa7f751c
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-13 10:36:51 +0100

    button downloads resume content as pdf

--- src/components/CenterTopBar.jsx
+++ src/components/CenterTopBar.jsx
@@ -1,9 +1,15 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useContext } from 'react'
 import { FaFileDownload } from 'react-icons/fa'
+import DataContext from '../Context'
 
 function centerTopBar() {
+  const { generatePDF } = useContext(DataContext)
   return (
     <>
-    <button className="download font-bold flex w-[100%] justify-between bg-white h-12 rounded-[10px] p-2 sticky top-[10px]">
+    <button
+    onClick={generatePDF}
+    className="download font-bold flex w-[100%] justify-between bg-white h-12 rounded-[10px] p-2 sticky top-[10px]">
       <p className='flex justify-center w-full pt-1'> 
         <FaFileDownload style= {{
           color: '#fff',
