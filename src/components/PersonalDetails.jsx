6364b52f4a63e36c987d45bf72b07daba04d1ab5
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-25 12:58:04 +0100

    added top margin

--- src/components/PersonalDetails.jsx
+++ src/components/PersonalDetails.jsx
@@ -2,7 +2,7 @@ import {Details} from '../components'
 
 export const PersonalDetails = () => {
   return (
-    <div className='h-[18%] p-3  border-b-[5px] mb-4  '>
+    <div className='h-[18%] p-3 mt-6  border-b-[5px] mb-4  '>
         <Details />
     </div>
   )
